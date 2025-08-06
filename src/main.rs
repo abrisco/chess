dfb660c87d929f6a34fdc253ce8236a0082e0087
Andre Brisco <andre.brisco@protonmail.com>
2025-08-05 20:08:20 -0700

    Added some form of camera rotation

--- src/main.rs
+++ src/main.rs
@@ -13,39 +13,16 @@ fn main() {
         .run();
 }
 
-#[derive(Debug, Resource)]
-struct CameraSettings {
-    pub orbit_distance: f32,
-    pub pitch_speed: f32,
-    // Clamp pitch to this range
-    pub pitch_range: Range<f32>,
-    pub roll_speed: f32,
-    pub yaw_speed: f32,
-}
-
-impl Default for CameraSettings {
-    fn default() -> Self {
-        // Limiting pitch stops some unexpected rotation past 90° up or down.
-        let pitch_limit = FRAC_PI_2 - 0.01;
-        Self {
-            // These values are completely arbitrary, chosen because they seem to produce
-            // "sensible" results for this example. Adjust as required.
-            orbit_distance: 20.0,
-            pitch_speed: 0.003,
-            pitch_range: -pitch_limit..pitch_limit,
-            roll_speed: 1.0,
-            yaw_speed: 0.004,
-        }
-    }
-}
+const CAMERA_START_X: f32 = 0.5;
+const CAMERA_START_Y: f32 = 0.5;
+const CAMERA_START_Z: f32 = 0.5;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, Self::startup)
-            .add_systems(Update, Self::update_camera)
-            .init_resource::<CameraSettings>();
+            .add_systems(Update, Self::update_camera);
     }
 }
 
@@ -54,6 +31,7 @@ impl GamePlugin {
         mut commands: Commands,
         mut meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<StandardMaterial>>,
+        asset_server: Res<AssetServer>,
     ) {
         // circular base
         commands.spawn((
@@ -61,24 +39,24 @@ impl GamePlugin {
             MeshMaterial3d(materials.add(Color::WHITE)),
             Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
         ));
-        // cube
-        commands.spawn((
-            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-            MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
-            Transform::from_xyz(0.0, 0.5, 0.0),
-        ));
+
+        // Board
+        commands.spawn(SceneRoot(asset_server.load(
+            GltfAssetLabel::Scene(0).from_asset("models/chess_board.glb")
+        )));
+
         // light
         commands.spawn((
             PointLight {
                 shadows_enabled: true,
                 ..default()
             },
-            Transform::from_xyz(4.0, 8.0, 4.0),
+            Transform::from_xyz(4.0, 8.0, 9.0),
         ));
         // camera
         commands.spawn((
             Camera3d::default(),
-            Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+            Transform::from_xyz(CAMERA_START_X, CAMERA_START_Y, CAMERA_START_Z).looking_at(Vec3::ZERO, Vec3::Y),
         ));
     }
 
@@ -88,15 +66,21 @@ impl GamePlugin {
         mouse_motion: Res<AccumulatedMouseMotion>,
         keyboard_input: Res<ButtonInput<KeyCode>>,
     ) {
+        // Placeholder constant focus
+        let focus = Vec3::ZERO;
+
         // Reset position when "R" is pressed
         if keyboard_input.pressed(KeyCode::KeyR) {
-            camera.translation = Vec3::new(-2.5, 4.5, 9.0);
-            camera.look_at(Vec3::ZERO, Vec3::Y);
+            camera.translation = Vec3::new(CAMERA_START_X, CAMERA_START_Y, CAMERA_START_Z);
+            camera.look_at(focus, Vec3::Y);
         }
+
+        // Constants
+        let sensitivity = 0.03;
 
         // If no mouse buttons are pressed, don't update the camera.
         let delta = if mouse_buttons.pressed(MouseButton::Left) {
-            mouse_motion.delta
+            mouse_motion.delta * 0.1
         } else if keyboard_input.any_pressed([
             KeyCode::ArrowUp,
             KeyCode::ArrowDown,
@@ -105,16 +89,16 @@ impl GamePlugin {
         ]) {
             Vec2::new(
                 if keyboard_input.pressed(KeyCode::ArrowRight) {
-                    3.0
+                    sensitivity
                 } else if keyboard_input.pressed(KeyCode::ArrowLeft) {
-                    -3.0
+                    -sensitivity
                 } else {
                     0.0
                 },
                 if keyboard_input.pressed(KeyCode::ArrowUp) {
-                    3.0
+                    sensitivity
                 } else if keyboard_input.pressed(KeyCode::ArrowDown) {
-                    -3.0
+                    -sensitivity
                 } else {
                     0.0
                 },
@@ -123,12 +107,6 @@ impl GamePlugin {
             return;
         };
 
-        // Constants
-        let sensitivity = 0.01;
-
-        // Placeholder constant focus
-        let focus = Vec3::ZERO;
-
         // Current offset from focus
         let offset = camera.translation - focus;
 
@@ -136,30 +114,19 @@ impl GamePlugin {
         let forward = -offset.normalize(); // viewing direction is toward focus
 
         // Use the camera's current local up to avoid instability
-        let local_up = camera.rotation * Vec3::Y;
-
-        // If forward is too close to up, fall back to another axis. This occurs because
-        // as two vectors approach being identical their cross product nears zero.
-        let pole_threshold = 0.999;
-        let fallback_up = camera.rotation * Vec3::Z;
-        let view_up = if forward.dot(local_up).abs() > pole_threshold {
-            fallback_up
-        } else {
-            local_up
-        };
-
-        let right = forward.cross(view_up).normalize();
-        let up = right.cross(forward).normalize();
+        let up = camera.rotation * Vec3::Y;
+        let right = forward.cross(up).normalize();
 
         // Compute pitch and yaw
-        let pitch = Quat::from_axis_angle(right, -delta.y * sensitivity);
-        let yaw = Quat::from_axis_angle(up, -delta.x * sensitivity);
+        let pitch = Quat::from_axis_angle(right, -delta.y);
+        let yaw = Quat::from_axis_angle(up, -delta.x);
         let rotation = yaw * pitch;
 
         // Update camera position
         camera.translation = focus + (rotation * offset);
 
-        // Update look_at using up as it's a safe reference point.
-        camera.look_at(focus, up);
+        // Set the new lookat after accounting for the change in yaw
+        camera.look_at(focus, pitch * up);
+        println!("{:?}", camera.translation);
     }
 }
